8542b9d160c395ab66e151b7edb1fd3c02c9b412
Anastasia Shcheblykina <anastasia23121993@gmail.com>
2023-02-26 20:14:42 +0300

    create test

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -2,7 +2,6 @@ package ru.netology.delivery.data;
 
 import com.github.javafaker.Faker;
 import lombok.Value;
-import lombok.val;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -15,7 +14,7 @@ public class DataGenerator {
     private DataGenerator() {
     }
 
-    public static String generateDate(int shift, String pattern) {
+    public static String generateDate(int shift) {
         return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
